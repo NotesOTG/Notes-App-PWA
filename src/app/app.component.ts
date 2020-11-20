1dee1755536a50ae8fc751bb41c6ddad0eb73289
tristan <wantedrednight@gmail.com>
2020-11-20 03:16:22 -0600

    Updating project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
-import { Component, isDevMode } from '@angular/core';
+import { Component, isDevMode, OnInit } from '@angular/core';
+import { Catergories } from './shared/models/categories';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'notes-app-pwa';
 
+  ngOnInit(): void {
+    //console.log('is dev mode? ', isDevMode());
+  }
+  
 }
--- src/app/modules/landing/landing.component.ts
+++ src/app/modules/landing/landing.component.ts
@@ -11,7 +11,6 @@ export class LandingComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
   
 
 }
